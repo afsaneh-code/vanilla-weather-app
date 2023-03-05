9ade33a568cafb84507ce4724994cce300c60f18
afsaneh-code <hosseini.afsaneh@yahoo.com>
2023-03-05 23:34:58 +0330

    completing css and html-2

--- vanilla-weather-afsoon app/src/app.js
+++ vanilla-weather-afsoon app/src/app.js
@@ -1,11 +1,15 @@
-function displayTemperature any[]:void
+function displayTemperature (response){
 console.log(Response.data);
 let temperatureElement=document.querySelector("#temperature");
 let cityElement=document.querySelector("#city");
+let descriptionElement=document.querySelector("#description");
+let humidityElement=document.querySelector("#humidity");
 temperatureElement.innerHTML=Math.round
 (Response.data,main.temp);
 cityElement.innerHTML=response.data.name;
+descriptionElement.innerHTML=response.data.weather[0].description;
 }
-let apiKey="";
-let apiUrl="";
+let apiKey="a80bc9ac92c9d9bb92139716051fba9a";
+let apiUrl="https://api.openweathermap.org/data/2.5/weather?q=London&appid={API key}";
+console.log(apiUrl);
 axios.get(apiUrl).then(displayTemperature);

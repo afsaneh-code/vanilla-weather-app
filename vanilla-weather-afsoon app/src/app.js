46fde926c44f0427a66bf6bcfc86d4087787172f
afsaneh-code <hosseini.afsaneh@yahoo.com>
2023-03-07 01:23:01 +0330

    completing css and html 3

--- vanilla-weather-afsoon app/src/app.js
+++ vanilla-weather-afsoon app/src/app.js
@@ -15,6 +15,7 @@ let descriptionElement=document.querySelector("#description");
 let humidityElement=document.querySelector("#humidity");
 let windElement=document.querySelector("#wind");
 let dateElement=document.querySelector("#date");
+let iconElement=document.querySelector("#icon");
 temperatureElement.innerHTML=Math.round
 (response.data.main.temp);
 cityElement.innerHTML=response.data.name;
@@ -22,10 +23,13 @@ descriptionElement.innerHTML=response.data.weather[0].description;
 humidityElement.innerHTML=response.data.main.humidity;
 windElement.innerHTML=Math.round(response.data.wind.speed);
 dateElement.innerHTML=formatdata(response.date.dt * 1000);
+iconElement.setAttribute("src",`https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+iconElement.setAttribute("alt", response.data.weather[0].description);
+
 }
 let apiKey="a80bc9ac92c9d9bb92139716051fba9a";
 console.log(apiKey)
 let city="London"
-let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=${apiKey}&units=metric`;
+let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=${city},uk&appid=${apiKey}&units=metric`;
 console.log(apiUrl);
 axios.get(apiUrl).then(displayTemperature);

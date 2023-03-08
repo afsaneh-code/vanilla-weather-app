ef1e5dfa174276d4b338e0be8e5b3eca160c8ee7
afsaneh-code <hosseini.afsaneh@yahoo.com>
2023-03-08 21:32:32 +0330

    completing css and html 3

--- vanilla-weather-afsoon app/src/app.js
+++ vanilla-weather-afsoon app/src/app.js
@@ -1,12 +1,26 @@
-function formatDate(timestamp){
-    let date= new Date(timestamp);
-    let hours= date.getHours();
-    let minutes= date.getMinutes();
-    let days= ["Sunday","Monday","Tuesday" ,"Wednesday" ,"Thursday","Friday","Saturday"
-];
-    let day=days[date.getDay()];
-    return `${day} ${hours} : ${minutes}`;
-}
+function formatDate(timestamp) {
+    let date = new Date(timestamp);
+    let hours = date.getHours();
+    if (hours < 10) {
+      hours = `0${hours}`;
+    }
+    let minutes = date.getMinutes();
+    if (minutes < 10) {
+      minutes = `0${minutes}`;
+    }
+  
+    let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+    let day = days[date.getDay()];
+    return `${day} ${hours}:${minutes}`;
+  }
 function displayTemperature (response){
 console.log(response.data);
 let temperatureElement=document.querySelector("#temperature");
@@ -16,20 +30,26 @@ let humidityElement=document.querySelector("#humidity");
 let windElement=document.querySelector("#wind");
 let dateElement=document.querySelector("#date");
 let iconElement=document.querySelector("#icon");
+celsiusTemperature = response.data.main.temp;
 temperatureElement.innerHTML=Math.round
-(response.data.main.temp);
+(celsiusTemperature);
 cityElement.innerHTML=response.data.name;
 descriptionElement.innerHTML=response.data.weather[0].description;
 humidityElement.innerHTML=response.data.main.humidity;
-windElement.innerHTML=Math.round(response.data.wind.speed);
+windElement.innerHTML=Math.round(response.data.wind.speed * 3.6);
 dateElement.innerHTML=formatdata(response.date.dt * 1000);
 iconElement.setAttribute("src",`https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
 iconElement.setAttribute("alt", response.data.weather[0].description);
-
 }
+function search(city){
 let apiKey="a80bc9ac92c9d9bb92139716051fba9a";
 console.log(apiKey)
-let city="London"
-let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=${city},uk&appid=${apiKey}&units=metric`;
+let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 console.log(apiUrl);
 axios.get(apiUrl).then(displayTemperature);
+}
+function handleSubmit(event) {
+    event.preventDefault();
+    let cityInputElement = document.querySelector("#city-input");
+    search(cityInputElement.value);
+  }
